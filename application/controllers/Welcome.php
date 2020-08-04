09b63ddc330ff8fbdc6eee0fbdcb67e11ee8b390
estigadotnet <project.estiga@gmail.com>
2020-08-05 01:34:07 +0700

    .

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// $this->load->view('welcome_message');
+		$data['_view'] = '0_welcome_message';
+		$this->load->view('_layout', $data);
 	}
 }
