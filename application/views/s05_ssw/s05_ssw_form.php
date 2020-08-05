f382e18c3aa6fa10d3d48c9e47bf6fa38a885b43
estigadotnet <project.estiga@gmail.com>
2020-08-06 00:09:47 +0700

    .

---
+++ application/views/s05_ssw/s05_ssw_form.php
@@ -0,0 +1,13 @@
+  <form action="<?php echo $action; ?>" method="post">
+	    <div class="form-group">
+            <label for="varchar">NIS <?php echo form_error('NIS') ?></label>
+            <input type="text" class="form-control" name="NIS" id="NIS" placeholder="NIS" value="<?php echo $NIS; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Nama <?php echo form_error('Nama') ?></label>
+            <input type="text" class="form-control" name="Nama" id="Nama" placeholder="Nama" value="<?php echo $Nama; ?>" />
+        </div>
+	    <input type="hidden" name="idssw" value="<?php echo $idssw; ?>" />
+	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button>
+	    <a href="<?php echo site_url('s05_ssw') ?>" class="btn btn-default">Cancel</a>
+	</form>
---
+++ application/views/s05_ssw/s05_ssw_read.php
@@ -0,0 +1,5 @@
+  <table class="table">
+	    <tr><td>NIS</td><td><?php echo $NIS; ?></td></tr>
+	    <tr><td>Nama</td><td><?php echo $Nama; ?></td></tr>
+	    <tr><td></td><td><a href="<?php echo site_url('s05_ssw') ?>" class="btn btn-default">Cancel</a></td></tr>
+	</table>
